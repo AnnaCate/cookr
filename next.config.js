5c48634b7853595676b7f91dd82f63ac48e1dcab
Anna Fulton <afulton7@gmail.com>
2022-03-06 13:48:00 -0500

    delete env.production to see if that fixes vercel problem

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  env: {
-    AUTH0_BASE_URL: process.env.VERCEL_URL || process.env.AUTH0_BASE_URL,
-  },
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    ...defaultConfig,
+    env: {
+      AUTH0_BASE_URL: process.env.VERCEL_URL || process.env.AUTH0_BASE_URL,
+    },
+  }
+  return nextConfig
 }
